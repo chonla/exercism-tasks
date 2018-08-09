c2037c0470ef6b609c8e2ffd408bae9ffb281c2e
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-08-09 10:13:02 +0700

    Replace regexp with byte range check

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -4,7 +4,6 @@ Package isogram provides a method to determine if a given text is isogram or not
 package isogram
 
 import (
-	"regexp"
 	"strings"
 )
 
@@ -12,16 +11,16 @@ import (
 func IsIsogram(text string) bool {
 	// Clean up
 	text = strings.ToLower(text)
-	s := regexp.MustCompile("[^a-z]").Split(text, -1)
-	text = strings.Join(s, "")
 
 	// Check
 	pool := map[byte]int{}
 	for i := range text {
-		if _, ok := pool[text[i]]; ok {
-			return false
+		if 'a' <= text[i] && text[i] <= 'z' {
+			if _, ok := pool[text[i]]; ok {
+				return false
+			}
+			pool[text[i]] = 1
 		}
-		pool[text[i]] = 1
 	}
 
 	return true
