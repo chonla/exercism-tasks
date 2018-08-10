7f8a74f7f9be5ca0a1565d397fcfae2b84d3fbac
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-08-10 11:44:10 +0700

    Add nucleotide-count

---
+++ go/nucleotide-count/nucleotide_count.go
@@ -0,0 +1,32 @@
+/*
+Package dna provides a method to count nucleotides in a valid DNA strand.
+*/
+package dna
+
+import "errors"
+
+// Histogram is a mapping from nucleotide to its count in given DNA.
+type Histogram map[rune]int
+
+// DNA is a list of nucleotides.
+type DNA string
+
+// Counts generates a histogram of valid nucleotides in the given DNA.
+// Returns an error if d contains an invalid nucleotide.
+func (d DNA) Counts() (Histogram, error) {
+	var h = Histogram{
+		'A': 0,
+		'C': 0,
+		'G': 0,
+		'T': 0,
+	}
+
+	for _, c := range d {
+		if _, ok := h[c]; !ok {
+			return nil, errors.New("invalid nucleotide found")
+		}
+		h[c]++
+	}
+
+	return h, nil
+}
