25b8caa72ffeb1746429801eceab5a837440555d
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-08-11 12:21:53 +0700

    Add Luhn

---
+++ go/luhn/luhn.go
@@ -0,0 +1,43 @@
+/*
+Package luhn provides method to validate if given sequence of number is a valid string based on Luhn algorithm.
+*/
+package luhn
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Valid return true if num is a valid string using Luhn algorithm, otherwise return false.
+func Valid(num string) bool {
+	num = strings.Replace(num, " ", "", -1)
+
+	n := len(num)
+	if n <= 1 {
+		return false
+	}
+
+	crc := 0
+	if n&1 == 1 {
+		num = fmt.Sprintf("0%s", num)
+		n = len(num)
+	}
+	for i, d := 0, 0; i < n; i++ {
+		if '0' <= num[i] && num[i] <= '9' {
+			if d&1 == 1 {
+				crc += int(num[i] - '0')
+			} else {
+				s := int(num[i]-'0') * 2
+				if s > 9 {
+					s -= 9
+				}
+				crc += s
+			}
+			d++
+		} else {
+			return false
+		}
+	}
+
+	return crc%10 == 0
+}
