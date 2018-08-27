990a1e1b7d28caf3e620472d9e81fae2383eb501
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-08-27 14:25:54 +0700

    Finish grains

---
+++ go/grains/grains.go
@@ -0,0 +1,95 @@
+/*
+Package grains provides methods to calculate number of grains put on chessboard.
+*/
+package grains
+
+import (
+	"errors"
+)
+
+// precalculated holds pre-calculated value to make calculation faster
+var precalculated = []uint64{
+	0,
+	1,
+	2,
+	4,
+	8,
+	16,
+	32,
+	64,
+	128,
+	256,
+	512,
+	1024,
+	2048,
+	4096,
+	8192,
+	16384,
+	32768,
+	65536,
+	131072,
+	262144,
+	524288,
+	1048576,
+	2097152,
+	4194304,
+	8388608,
+	16777216,
+	33554432,
+	67108864,
+	134217728,
+	268435456,
+	536870912,
+	1073741824,
+	2147483648,
+	4294967296,
+	8589934592,
+	17179869184,
+	34359738368,
+	68719476736,
+	137438953472,
+	274877906944,
+	549755813888,
+	1099511627776,
+	2199023255552,
+	4398046511104,
+	8796093022208,
+	17592186044416,
+	35184372088832,
+	70368744177664,
+	140737488355328,
+	281474976710656,
+	562949953421312,
+	1125899906842624,
+	2251799813685248,
+	4503599627370496,
+	9007199254740992,
+	18014398509481984,
+	36028797018963968,
+	72057594037927936,
+	144115188075855872,
+	288230376151711744,
+	576460752303423488,
+	1152921504606846976,
+	2305843009213693952,
+	4611686018427387904,
+	9223372036854775808,
+}
+
+// Square returns the number of grain put on input-th index square on chessboard.
+func Square(input int) (uint64, error) {
+	if 0 < input && input <= 64 {
+		return precalculated[input], nil
+	}
+	return 0, errors.New("input should be greater than 0 and less than or equal to 64")
+}
+
+// Total returns the total number of grains on a chessboard.
+func Total() uint64 {
+	sum := uint64(0)
+	for i := 1; i <= 64; i++ {
+		sq, _ := Square(i)
+		sum += sq
+	}
+	return sum
+}
