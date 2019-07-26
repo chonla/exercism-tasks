1066566780941e69bb64b9e65938cddf2301a669
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-07-26 07:37:47 +0700

    Complete pangram

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -6,14 +6,15 @@ import Data.Set as S
 
 alphabet :: [Char]
 alphabet = ['a'..'z']
+
 alphabetSet :: Set Char
 alphabetSet = S.fromList alphabet
 
-extractAlphabets :: String -> String
-extractAlphabets text = L.filter (`elem` alphabet) text
+extractAlphabets :: String -> Set Char
+extractAlphabets text = S.fromList (L.filter (`elem` alphabet) text)
 
 lowerString :: String -> String
 lowerString text = L.map C.toLower text
 
 isPangram :: String -> Bool
-isPangram text = (S.fromList (L.map head (L.group (L.sort (extractAlphabets (lowerString text)))))) == alphabetSet
+isPangram text = (extractAlphabets (lowerString text)) == alphabetSet
