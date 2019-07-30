cedce6ecb6ac2cebf54b8e13dec5f2ed12743333
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-07-31 00:43:50 +0700

    Add partially solved task

---
+++ haskell/queen-attack/src/Queens.hs
@@ -0,0 +1,13 @@
+module Queens (boardString, canAttack) where
+
+put :: Char -> Int -> Int -> String -> String
+put bw locY locX board = (take ((locY * 16) + (locX * 2)) board) ++ [bw] ++ (drop ((locY * 16) + (locX * 2 + 1)) board)
+
+boardString :: Maybe (Int, Int) -> Maybe (Int, Int) -> String
+boardString Nothing Nothing = concat $ (replicate 8 "_ _ _ _ _ _ _ _\n")
+boardString (Just (wx, wy)) Nothing = put 'W' wx wy (boardString Nothing Nothing)
+boardString Nothing (Just (bx, by)) = put 'B' bx by (boardString Nothing Nothing)
+boardString (Just (wx, wy)) (Just (bx, by)) = put 'W' wx wy (put 'B' bx by (boardString Nothing Nothing))
+
+canAttack :: (Int, Int) -> (Int, Int) -> Bool
+canAttack queenA queenB = error "You need to implement this function."
