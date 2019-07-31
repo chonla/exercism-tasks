1cbe1c5c6f11ceb44644e5c5e5b4f1289801f5eb
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-08-01 00:23:39 +0700

    Complete hamming

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -1,4 +1,12 @@
 module Hamming (distance) where
 
+import Data.Maybe
+
 distance :: String -> String -> Maybe Int
-distance xs ys = error "You need to implement this function."
+distance [] [] = Just 0
+distance xs [] = Nothing
+distance [] ys = Nothing
+distance (x:xs) (y:ys)
+    | length xs /= length ys = Nothing
+    | x == y = Just (0 + fromJust (distance xs ys))
+    | otherwise = Just (1 + fromJust (distance xs ys))
