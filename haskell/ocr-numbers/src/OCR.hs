1cbad5b345f4577f973dac15dd599603df79f023
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-08-03 00:32:57 +0700

    Use my own chunks

--- haskell/ocr-numbers/src/OCR.hs
+++ haskell/ocr-numbers/src/OCR.hs
@@ -2,14 +2,9 @@ module OCR (convert) where
 
 import Data.List
 
-build :: ((a -> [a] -> [a]) -> [a] -> [a]) -> [a]
-build g = g (:) []
-
-chunksOf :: Int -> [e] -> [[e]]
-chunksOf i ls = map (take i) (build (splitter ls)) where
-  splitter :: [e] -> ([e] -> a -> a) -> a -> a
-  splitter [] _ n = n
-  splitter l c n  = l `c` splitter (drop i l) c n
+chunks :: Int -> String -> [String]
+chunks _ [] = []
+chunks n xs = (take n xs) : (chunks n (drop n xs))
 
 decode :: String -> Char
 decode " || _ _  || " = '0'
@@ -33,6 +28,6 @@ convert xs
     | length (rotatedNumbers!!0) == 4 = recognize preparedNumbers
     | otherwise = convert (unlines (take 4 (lines xs))) ++ "," ++ convert (unlines (drop 4 (lines xs)))
     where
-        preparedNumbers = chunksOf 12 (intercalate "" rotatedNumbers)
+        preparedNumbers = chunks 12 (intercalate "" rotatedNumbers)
         rotatedNumbers = transpose (lines xs)
 
