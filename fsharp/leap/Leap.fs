1dd750191e1bda1746a38ac752676b3f837b1bdc
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-08-21 21:05:03 +0700

    Fix argument declaration, Use infix

--- fsharp/leap/Leap.fs
+++ fsharp/leap/Leap.fs
@@ -1,10 +1,10 @@
 ﻿module Leap
 
-let isDivisibleBy (year: int, num: int): bool = year % num = 0
+let isDivisibleBy (year: int) (num: int): bool = year % num = 0
 
 let leapYear (year: int): bool = 
     match year with
-    | year when isDivisibleBy (year, 400) -> true
-    | year when isDivisibleBy (year, 100) -> false
-    | year when isDivisibleBy (year, 4) -> true
+    | year when year |> isDivisibleBy <| 400 -> true
+    | year when year |> isDivisibleBy <| 100 -> false
+    | year when year |> isDivisibleBy <| 4 -> true
     | _ -> false
