2da899268e1d9a6c8a6af5950c8f5bae951fe53e
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-08-21 13:15:36 +0700

    Refactor guard

--- fsharp/leap/Leap.fs
+++ fsharp/leap/Leap.fs
@@ -1,8 +1,10 @@
 ﻿module Leap
 
+let isDivisibleBy (year: int, num: int): bool = year % num = 0
+
 let leapYear (year: int): bool = 
     match year with
-    | year when year % 400 = 0 -> true
-    | year when year % 100 = 0 -> false
-    | year when year % 4 = 0 -> true
+    | year when isDivisibleBy (year, 400) -> true
+    | year when isDivisibleBy (year, 100) -> false
+    | year when isDivisibleBy (year, 4) -> true
     | _ -> false
