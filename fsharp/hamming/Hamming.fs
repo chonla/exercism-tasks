dba2d38996d71461ddd890d08869e046cbb4f4c0
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-08-22 18:59:27 +0700

    Add hamming

---
+++ fsharp/hamming/Hamming.fs
@@ -0,0 +1,15 @@
+﻿module Hamming
+
+let tally (x: int option): int option =
+    match x with
+    | Some v -> Some (v + 1)
+    | None -> None
+
+let rec distance (strand1: string) (strand2: string): int option = 
+    match [strand1; strand2] with
+    | [strand1; strand2] when strand1 = "" && strand2 = "" -> Some 0
+    | [strand1; strand2] when strand1 = "" && strand2 <> "" -> None
+    | [strand1; strand2] when strand1 <> "" && strand2 = "" -> None
+    | [strand1; strand2] when strand1.[0] = strand2.[0] -> distance strand1.[1..] strand2.[1..]
+    | [strand1; strand2] when strand1.[0] <> strand2.[0] -> tally (distance strand1.[1..] strand2.[1..])
+    | _ -> Some 0
