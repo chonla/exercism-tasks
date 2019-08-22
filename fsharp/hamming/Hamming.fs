61218041573c319be6b7bcbd5448e8ab23b50f4b
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-08-22 20:00:23 +0700

    Improve hamming

--- fsharp/hamming/Hamming.fs
+++ fsharp/hamming/Hamming.fs
@@ -1,15 +1,6 @@
 ﻿module Hamming
 
-let tally (x: int option): int option =
-    match x with
-    | Some v -> Some (v + 1)
-    | None -> None
-
 let rec distance (strand1: string) (strand2: string): int option = 
-    match [strand1; strand2] with
-    | [strand1; strand2] when strand1 = "" && strand2 = "" -> Some 0
-    | [strand1; strand2] when strand1 = "" && strand2 <> "" -> None
-    | [strand1; strand2] when strand1 <> "" && strand2 = "" -> None
-    | [strand1; strand2] when strand1.[0] = strand2.[0] -> distance strand1.[1..] strand2.[1..]
-    | [strand1; strand2] when strand1.[0] <> strand2.[0] -> tally (distance strand1.[1..] strand2.[1..])
-    | _ -> Some 0
+    match strand1.Length = strand2.Length with
+    | false -> None
+    | true -> Some (Seq.length (Seq.filter (fun (c1, c2) -> c1 <> c2) (Seq.zip strand1 strand2)))
