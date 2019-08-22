b79b376c5ffad5f02e5dfb3fd666dd83dfc2ac92
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-08-22 20:46:12 +0700

    Add pangram

---
+++ fsharp/pangram/Pangram.fs
@@ -0,0 +1,6 @@
+﻿module Pangram
+
+let alphabet = Set.ofSeq['a'..'z']
+
+let isPangram (input: string): bool = 
+    alphabet = Set.filter (fun (c) -> alphabet.Contains(c)) (Set.ofArray(input.ToLower().ToCharArray()))
