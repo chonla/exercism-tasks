d85e2e9b100f7d5f6af2a1ba03f322761fa58bb0
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-08-22 20:55:22 +0700

    Remove use of alphabet

--- fsharp/pangram/Pangram.fs
+++ fsharp/pangram/Pangram.fs
@@ -2,10 +2,9 @@
 
 open System
 
-let alphabet = Set.ofSeq['a'..'z']
-
 let isPangram (input: string): bool = 
     input.ToLower().ToCharArray()
         |> Set.ofArray
         |> Set.filter Char.IsLetter
-        |> (=) alphabet
+        |> Set.count
+        |> (=) 26
