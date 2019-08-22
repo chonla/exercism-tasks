07a7960209880751c3f6cdd0cb2b498e9900f94e
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-08-22 14:05:54 +0700

    Use composite condition instead of guard

--- fsharp/leap/Leap.fs
+++ fsharp/leap/Leap.fs
@@ -3,8 +3,6 @@
 let isDivisibleBy (year: int) (num: int): bool = year % num = 0
 
 let leapYear (year: int): bool = 
-    match year with
-    | year when year |> isDivisibleBy <| 400 -> true
-    | year when year |> isDivisibleBy <| 100 -> false
-    | year when year |> isDivisibleBy <| 4 -> true
-    | _ -> false
+    year |> isDivisibleBy <| 4 && (
+        not (year |> isDivisibleBy <| 100) || year |> isDivisibleBy <| 400
+    )
