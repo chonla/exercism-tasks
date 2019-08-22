71eb57c9c6a690e63a1aa13aab056e5b6623832b
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-08-22 20:50:30 +0700

    Improve readability of pangram

--- fsharp/pangram/Pangram.fs
+++ fsharp/pangram/Pangram.fs
@@ -3,4 +3,7 @@
 let alphabet = Set.ofSeq['a'..'z']
 
 let isPangram (input: string): bool = 
-    alphabet = Set.filter (fun (c) -> alphabet.Contains(c)) (Set.ofArray(input.ToLower().ToCharArray()))
+    input.ToLower().ToCharArray()
+        |> Set.ofArray
+        |> Set.filter (fun (c) -> alphabet.Contains(c))
+        |> (=) alphabet
