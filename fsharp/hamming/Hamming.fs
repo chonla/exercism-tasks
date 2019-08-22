a040ede0f9c91e410ac3614bc9b6c7ad315f54d7
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-08-22 20:15:56 +0700

    Remove recursive notation

--- fsharp/hamming/Hamming.fs
+++ fsharp/hamming/Hamming.fs
@@ -1,6 +1,6 @@
 ﻿module Hamming
 
-let rec distance (strand1: string) (strand2: string): int option = 
+let distance (strand1: string) (strand2: string): int option = 
     match strand1.Length = strand2.Length with
     | false -> None
     | true -> Some (Seq.length (Seq.filter (fun (c1, c2) -> c1 <> c2) (Seq.zip strand1 strand2)))
