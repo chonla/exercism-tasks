30bd4f873ebde600902c1951fd797bc9dc6f78df
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-08-23 09:54:28 +0700

    Add Robot Name

---
+++ fsharp/robot-name/RobotName.fs
@@ -0,0 +1,23 @@
+﻿module RobotName
+
+open System
+
+type Bot = { Name: string }
+
+let alphabet: char list = ['A'..'Z']
+let number: char list = ['0'..'9']
+
+let createName(): string = 
+    String([|
+        alphabet.[Random().Next() % 26];
+        alphabet.[Random().Next() % 26];
+        number.[Random().Next() % 10];
+        number.[Random().Next() % 10];
+        number.[Random().Next() % 10]
+    |])
+
+let mkRobot(): Bot = { Name = createName() }
+
+let name (robot: Bot): string = robot.Name
+
+let reset (robot: Bot) = mkRobot()
