36dbd564f6964a4aa554622012b0d5c94b707829
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-08-23 10:29:13 +0700

    Add Two-Fer

---
+++ fsharp/two-fer/TwoFer.fs
@@ -0,0 +1,9 @@
+﻿module TwoFer
+
+let nameOf (input: string option): string =
+    match input with
+    | Some v -> v
+    | _ -> "you"
+
+let twoFer (input: string option): string = 
+    "One for " + (nameOf input) + ", one for me."
