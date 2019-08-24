7a2e8efbf486ca4e17eb8f667ccda08e29a8293d
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-08-24 19:03:19 +0700

    Use Option.defaultValue instead of defaultArg

--- fsharp/two-fer/TwoFer.fs
+++ fsharp/two-fer/TwoFer.fs
@@ -1,4 +1,4 @@
 ﻿module TwoFer
 
 let twoFer (input: string option): string = 
-    "One for " + (defaultArg input "you") + ", one for me."
+    "One for " + (Option.defaultValue "you" input) + ", one for me."
