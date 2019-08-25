c6ad53b751c7a7fb62c3f1a19ae33b764d710437
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-08-25 08:11:27 +0700

    Add queen attack

---
+++ fsharp/queen-attack/QueenAttack.fs
@@ -0,0 +1,6 @@
+﻿module QueenAttack
+
+let create ((r, c): int * int) = ((0 <= r) && (r <= 7)) && ((0 <= c) && (c <= 7))
+
+let canAttack ((r1, c1): int * int) ((r2, c2): int * int) = 
+    (c1 = c2) || (r1 = r2) || (abs((r1 - r2) / (c1 - c2)) = 1)
