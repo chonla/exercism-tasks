369651693b7f286be26bcf70949f0c2106ad7842
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-08-28 01:05:20 +0700

    Complete raindrops

---
+++ fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,9 @@
+﻿module Raindrops
+
+let drop = Map.empty.Add(3, "Pling").Add(5, "Plang").Add(7, "Plong")
+
+let convert (number: int): string =
+    let r = Array.map (fun num -> drop.[num]) ( Array.filter (fun num -> number % num = 0) [| 3; 5; 7 |] )
+    match Array.isEmpty(r) with
+    | true -> number.ToString()
+    | _ -> String.concat "" r
