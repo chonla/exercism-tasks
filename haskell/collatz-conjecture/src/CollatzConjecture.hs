5c52729e687235e351961cfe37419e01f97a0f37
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-08-29 12:55:27 +0700

    Improve collatz conjecture by using iterate

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -5,15 +5,7 @@ evalCollatz num
     | even num = num `div` 2
     | otherwise = (num * 3) + 1
 
-buildCollatzSequence :: Integer -> [Integer]
-buildCollatzSequence num
-    | c == 1 = [c]
-    | otherwise = c : (buildCollatzSequence c)
-        where
-            c = evalCollatz num
-
 collatz :: Integer -> Maybe Integer
 collatz num
     | num <= 0 = Nothing
-    | num == 1 = Just 0
-    | otherwise = Just (fromIntegral (length (buildCollatzSequence num)))
+    | otherwise = Just (fromIntegral (length (takeWhile (>1) (iterate (evalCollatz) num))))
