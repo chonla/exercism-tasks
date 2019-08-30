28b9f977ca09edc3a1ba2339ffe4bbfd8e74fe51
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-08-30 23:32:55 +0700

    Improve readability

--- fsharp/raindrops/Raindrops.fs
+++ fsharp/raindrops/Raindrops.fs
@@ -3,8 +3,12 @@
 let drop: Map<int,string> = Map.ofSeq([(3, "Pling");(5, "Plang");(7, "Plong")])
 
 let convert (number: int): string =
-    let dropkeys = Array.map (fun (k, v) -> k) (Map.toArray (drop))
-    let r = Array.map (fun num -> drop.[num]) (Array.filter (fun num -> number % num = 0) dropkeys)
+    let r = 
+        drop 
+        |> Map.toArray
+        |> Array.map (fun (k, v) -> k)
+        |> Array.filter (fun num -> number % num = 0)
+        |> Array.map (fun num -> drop.[num])
     if Array.isEmpty(r) then
         number.ToString()
     else
