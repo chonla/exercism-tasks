b732c0fb2bf6acf3c12320320bce9c6280624f40
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-09-01 23:30:16 +0700

    Use constant instead of adhoc to increase readability

--- elixir/rna-transcription/lib/rna_transcription.ex
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -7,8 +7,10 @@ defmodule RnaTranscription do
   iex> RnaTranscription.to_rna('ACTG')
   'UGAC'
   """
+  @dna_map %{?G=>?C, ?C=>?G, ?T=>?A, ?A=>?U}
+  
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-    Enum.map(dna, fn d -> %{?G=>?C, ?C=>?G, ?T=>?A, ?A=>?U}[d] end)
+    Enum.map(dna, fn d -> @dna_map[d] end)
   end
 end
