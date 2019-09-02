dc3a3378ea13409e79522b7f834dfdd51e01d9fc
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-09-02 23:41:37 +0700

    Using tail recursive

--- fsharp/accumulate/Accumulate.fs
+++ fsharp/accumulate/Accumulate.fs
@@ -1,6 +1,7 @@
 ﻿module Accumulate
 
-let rec accumulate (func: 'a -> 'b) (input: 'a list): 'b list = 
-    match input with
-    | [] -> List.empty
-    | _ -> func(input.[0]) :: (accumulate (func) (List.tail(input)))
+let accumulate (func: 'a -> 'b) (input: 'a list): 'b list = 
+    let rec mapAccum f acc = function
+        | [] -> acc
+        | head::tail -> mapAccum f (f head :: acc) tail
+    mapAccum func [] input |> List.rev
