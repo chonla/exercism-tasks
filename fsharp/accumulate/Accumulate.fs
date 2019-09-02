ed1e4e619de0caab9956b628d24142216f564b0a
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-09-02 17:11:31 +0700

    Add accumulate f#

---
+++ fsharp/accumulate/Accumulate.fs
@@ -0,0 +1,7 @@
+﻿module Accumulate
+
+let rec accumulate (func: 'a -> 'b) (input: 'a list): 'b list = 
+    if List.isEmpty(input) then
+        List.empty
+    else
+        func(input.[0]) :: (accumulate (func) (List.tail(input)))
