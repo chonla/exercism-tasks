436e823ab80ef71a5cc7130e9627ddd1608b0607
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-09-04 08:34:35 +0800

    Refactor

--- elixir/word-count/lib/words.ex
+++ elixir/word-count/lib/words.ex
@@ -9,13 +9,18 @@ defmodule Words do
   @spec count(String.t()) :: map
   def count(sentence) do
     sentence
-      |> String.downcase()
       |> words
-      |> List.foldl(%{}, fn word, acc -> tally(word, acc) end)
+      |> group
+  end
+
+  defp group(words) do
+    List.foldl(words, %{}, fn word, acc -> tally(word, acc) end)
   end
 
   defp words(line) do
-    String.split(line, @unword_chars, trim: true)
+    line
+    |> String.downcase()
+    |> String.split(@unword_chars, trim: true)
   end
 
   defp tally(word, counter) do
