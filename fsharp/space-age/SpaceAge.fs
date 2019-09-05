4e539b3d5dc04ee9a642321ea25c6afcaa3e1f8f
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-09-06 06:32:50 +0800

    Use dict instead of array

--- fsharp/space-age/SpaceAge.fs
+++ fsharp/space-age/SpaceAge.fs
@@ -10,18 +10,18 @@ type Planet =
     | Uranus
     | Neptune
 
+[<Literal>]
+let SecondsInEarthDay = 31557600.0
+
 let earthYear =
-    [| (Earth, 1.0)
-       (Mercury, 0.2408467)
-       (Venus, 0.61519726)
-       (Mars, 1.8808158)
-       (Jupiter, 11.862615)
-       (Saturn, 29.447498)
-       (Uranus, 84.016846)
-       (Neptune, 164.79132) |]
+    dict [ Earth, 1.0
+           Mercury, 0.2408467
+           Venus, 0.61519726
+           Mars, 1.8808158
+           Jupiter, 11.862615
+           Saturn, 29.447498
+           Uranus, 84.016846
+           Neptune, 164.79132 ]
 
 let age (planet : Planet) (seconds : int64) : float =
-    earthYear
-    |> Array.filter (fun (k, v) -> k = planet)
-    |> Array.map (fun (k, v) -> (float (seconds) / v) / float (31557600))
-    |> Array.head
+    (float(seconds)/ earthYear.Item(planet))/SecondsInEarthDay
