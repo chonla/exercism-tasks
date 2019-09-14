31c8824b9ebea30e35d9cec18d2d375224aa6a4e
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-09-14 13:12:02 +0700

    Use Map.tryFind instead of object.tryFind and if-else

--- fsharp/grade-school/GradeSchool.fs
+++ fsharp/grade-school/GradeSchool.fs
@@ -5,19 +5,19 @@ type School = Map<int, string list>
 let empty : School = Map.empty
 
 let add (student : string) (grade : int) (school : School) : School =
-    if school.ContainsKey(grade) then
-        let students = school.[grade]
-        school.Add(grade, List.sort (student :: students))
-    else school.Add(grade, [ student ])
+    let students = Map.tryFind grade school
+    match students with
+    | Some v -> Map.add grade (List.sort (student :: Option.get(students))) school
+    | None -> Map.add grade [student] school
 
 let roster (school : School) : string list =
     school
     |> Map.toList
-    |> List.map (fun (k, v) -> v)
+    |> List.map snd
     |> List.fold List.append []
 
 let grade (number : int) (school : School) : string list =
-    let students = school.TryFind number
+    let students = Map.tryFind number school
     match students with
     | Some v -> v
     | None -> []
