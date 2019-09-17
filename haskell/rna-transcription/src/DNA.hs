95904a0914179bdccf336b8559926ecb16687575
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-09-17 21:24:12 +0700

    Use fmap to interact with Functor

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,7 +1,5 @@
 module DNA (toRNA) where
 
-import Data.Maybe
-
 dna :: Char -> Maybe Char
 dna 'C' = Just 'G'
 dna 'G' = Just 'C'
@@ -10,9 +8,8 @@ dna 'A' = Just 'U'
 dna _ = Nothing
 
 toRNA :: String -> Either Char String
-toRNA x 
-    | firstFail /= [] = Left (fst (head firstFail))
-    | otherwise = Right (map (fromJust . snd) transcription)
-    where
-        transcription = zip x (fmap (dna) x)
-        firstFail = filter (\p -> snd p == Nothing) transcription
+toRNA [] = Right []
+toRNA (x:xs) =
+    case dna x of
+        Just c -> fmap ([c] ++) (toRNA xs)
+        Nothing -> Left x
