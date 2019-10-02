3dace867558b7560e97adb63399e7b36b590004c
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-10-02 17:09:37 +0700

    Remove using of regex and use simpler functions

--- elixir/bob/lib/bob.ex
+++ elixir/bob/lib/bob.ex
@@ -1,16 +1,16 @@
 defmodule Bob do
-  defp is_saying_something(input), do: String.match?(input, ~r/[[:alpha:]]/)
-  defp is_shouting(input), do: not String.match?(input, ~r/[[:lower:]]/)
-  defp is_questioning(input), do: String.match?(input, ~r/\?$/)
-  defp is_silence(input), do: String.match?(input, ~r/^\s*$/)
+  defp saying_something(input), do: String.upcase(input) != String.downcase(input)
+  defp shouting(input), do: String.upcase(input) == input
+  defp questioning(input), do: String.ends_with?(input, "?")
+  defp silence(input), do: String.trim(input) == ""
 
   @spec hey(binary) :: <<_::72>>
   def hey(input) do
     cond do
-      is_silence(input) -> "Fine. Be that way!"
-      is_saying_something(input) && is_questioning(input) && is_shouting(input) -> "Calm down, I know what I'm doing!"
-      is_questioning(input) -> "Sure."
-      is_saying_something(input) && is_shouting(input) -> "Whoa, chill out!"
+      silence(input) -> "Fine. Be that way!"
+      saying_something(input) && questioning(input) && shouting(input) -> "Calm down, I know what I'm doing!"
+      questioning(input) -> "Sure."
+      saying_something(input) && shouting(input) -> "Whoa, chill out!"
       true -> "Whatever."
     end
   end
