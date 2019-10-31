433d5c5f3a00bca1ae0c63e5f589fd3a85f1252a
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-10-31 15:08:13 +0700

    Improve Bob in F#

--- fsharp/bob/Bob.fs
+++ fsharp/bob/Bob.fs
@@ -1,25 +1,28 @@
 ﻿module Bob
 
-let (|Shouting|_|) (input: string): Option<unit> = 
-    if input.Length > 0 && input.ToUpper () = input then Some Shouting else None
-
-let (|Questioning|_|) (input: string): Option<unit> =
-    if input.EndsWith "?" then Some Questioning else None
-
-let (|Saying|_|) (input: string): Option<unit> =
-    if input.Length > 1 then Some Saying else None
-
 let removeNonwordChars (input: string): string =
     input
     |> String.filter (fun c -> System.Char.IsLetter c || c = '?')
 
+let (|Shouting|_|) (input: string): Option<unit> = 
+    let cleanInput = removeNonwordChars input
+    if cleanInput.Length > 0 && cleanInput.ToUpper () = cleanInput then Some () else None
+
+let (|Questioning|_|) (input: string): Option<unit> =
+    let cleanInput = removeNonwordChars input
+    if cleanInput.EndsWith "?" then Some () else None
+
+let (|Saying|_|) (input: string): Option<unit> =
+    let cleanInput = removeNonwordChars input
+    if cleanInput.Length > 1 then Some () else None
+
+let (|Nothing|_|) (input: string): Option<unit> =
+    if input.Trim () = "" then Some () else None
+
 let response (input: string): string = 
-    if input.Trim () = "" then
-        "Fine. Be that way!"
-    else
-        let cleanInput = removeNonwordChars input
-        match cleanInput with
-        | Saying & Shouting & Questioning -> "Calm down, I know what I'm doing!"
-        | Questioning -> "Sure."
-        | Shouting -> "Whoa, chill out!"
-        | _ -> "Whatever."
+    match input with
+    | Nothing -> "Fine. Be that way!"
+    | Saying & Shouting & Questioning -> "Calm down, I know what I'm doing!"
+    | Questioning -> "Sure."
+    | Shouting -> "Whoa, chill out!"
+    | _ -> "Whatever."
