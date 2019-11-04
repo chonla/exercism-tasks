bb3aa10a4dee447217f82cde6fd68b78fe226db0
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-11-04 14:48:26 +0700

    Remove unused comment

--- scala/leap/src/main/scala/Leap.scala
+++ scala/leap/src/main/scala/Leap.scala
@@ -2,12 +2,4 @@ object Leap {
   def divisibleBy(dividend: Int, divisor: Int): Boolean = dividend % divisor == 0
 
   def leapYear(year: Int): Boolean = divisibleBy(year, 400) || (!divisibleBy(year, 100) && divisibleBy(year, 4))
-  // {
-  //   year match {
-  //     case year if divisibleBy(year, 400) => true
-  //     case year if divisibleBy(year, 100) => false
-  //     case year if divisibleBy(year, 4) => true
-  //     case _ => false
-  //   }
-  // }
 }
