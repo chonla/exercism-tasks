a6cc85401be147c2058f4e40427ddd36437e042b
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-11-04 20:05:56 +0700

    Improve readability using multiline string

--- elixir/beer-song/lib/beer_song.ex
+++ elixir/beer-song/lib/beer_song.ex
@@ -1,38 +1,34 @@
 defmodule BeerSong do
-  def beer_on_the_wall_verse(1) do
-    "1 bottle of beer on the wall, 1 bottle of beer.\n"
-  end
-
-  def beer_on_the_wall_verse(0) do
-    "No more bottles of beer on the wall, no more bottles of beer.\n"
-  end
-
-  def beer_on_the_wall_verse(number) do
-    "#{number} bottles of beer on the wall, #{number} bottles of beer.\n"
-  end
-
-  def beer_left_on_the_wall_verse(0) do
-    "Take it down and pass it around, no more bottles of beer on the wall.\n"
-  end
-
-  def beer_left_on_the_wall_verse(1) do
-    "Take one down and pass it around, 1 bottle of beer on the wall.\n"
-  end
-
-  def beer_left_on_the_wall_verse(number) when number < 0 do
-      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-  end
-
-  def beer_left_on_the_wall_verse(number) do
-    "Take one down and pass it around, #{number} bottles of beer on the wall.\n"
-  end
-
   @doc """
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t()
+  def verse(1) do
+    """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
+  end
+
+  def verse(0) do
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+  end
+
+  def verse(number) when number == 2 do
+    """
+    #{number} bottles of beer on the wall, #{number} bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    """
+  end
+
   def verse(number) do
-    beer_on_the_wall_verse(number) <> beer_left_on_the_wall_verse(number - 1)
+    """
+    #{number} bottles of beer on the wall, #{number} bottles of beer.
+    Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+    """
   end
 
   @doc """
