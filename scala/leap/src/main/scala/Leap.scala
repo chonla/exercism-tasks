37a576ac270ed1d7b25ffe1857869e6d24d0eb47
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-11-04 14:12:57 +0700

    Change leap year checking to one-liner

--- scala/leap/src/main/scala/Leap.scala
+++ scala/leap/src/main/scala/Leap.scala
@@ -1,12 +1,13 @@
 object Leap {
   def divisibleBy(dividend: Int, divisor: Int): Boolean = dividend % divisor == 0
 
-  def leapYear(year: Int): Boolean = {
-    year match {
-      case year if divisibleBy(year, 400) => true
-      case year if divisibleBy(year, 100) => false
-      case year if divisibleBy(year, 4) => true
-      case _ => false
-    }
-  }
+  def leapYear(year: Int): Boolean = divisibleBy(year, 400) || (!divisibleBy(year, 100) && divisibleBy(year, 4))
+  // {
+  //   year match {
+  //     case year if divisibleBy(year, 400) => true
+  //     case year if divisibleBy(year, 100) => false
+  //     case year if divisibleBy(year, 4) => true
+  //     case _ => false
+  //   }
+  // }
 }
