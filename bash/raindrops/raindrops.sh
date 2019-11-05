1b5c38832e0d505d30b530dd8e4a0fee4e5b7762
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-11-05 21:21:52 +0700

    Improve raindrops for bash, I guess

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
 main() {
-    buff=()
-    if (( $1 % 3 == 0 || $1 % 5 == 0 || $1 % 7 ==0 )); then
-        if (( $1 % 3 == 0 )); then
-            buff+=('Pling')
-        fi
-        if (( $1 % 5 == 0 )); then
-            buff+=('Plang')
-        fi
-        if (( $1 % 7 == 0 )); then
-            buff+=('Plong')
-        fi
-    else
-        buff+=($1)
+    if (( $1 % 3 != 0 && $1 % 5 != 0 && $1 % 7 != 0 )); then
+        echo $1
+        exit 0
     fi
-    out=$(IFS= ; echo "${buff[*]}")
-    echo "$out"
+    
+    if (( $1 % 3 == 0 )); then
+        printf "Pling"
+    fi
+    if (( $1 % 5 == 0 )); then
+        printf "Plang"
+    fi
+    if (( $1 % 7 == 0 )); then
+        printf "Plong"
+    fi
+    echo ""
 }
 
 main "$@"
