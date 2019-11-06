4834d2f4106ab5bcdc8009890595e2889f49d858
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-11-06 15:12:25 +0700

    Use robot struct

--- elixir/robot-simulator/lib/robot_simulator.ex
+++ elixir/robot-simulator/lib/robot_simulator.ex
@@ -12,6 +12,10 @@ defmodule RobotGuard do
                   is_integer(elem(position, 1))
 end
 
+defmodule Robot do
+  defstruct [dir: :north, pos: {0, 0}]
+end
+
 defmodule RobotSimulator do
   import RobotGuard
 
@@ -28,11 +32,11 @@ defmodule RobotSimulator do
 
   Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
-  @spec create(dir :: atom, pos :: {integer, integer}) :: any
+  @spec create(dir :: atom, pos :: {integer, integer}) :: Robot | {:error, String.t()}
   def create(dir \\ :north, pos \\ {0, 0})
 
   def create(dir, pos) when valid_dir(dir) and valid_position(pos) do
-    %{:dir => dir, :pos => pos}
+    %Robot{:dir => dir, :pos => pos}
   end
 
   def create(dir, _) when not valid_dir(dir) do
@@ -48,65 +52,68 @@ defmodule RobotSimulator do
 
   Valid instructions are: "R" (turn right), "L", (turn left), and "A" (advance)
   """
-  @spec simulate(robot :: any, instructions :: String.t()) :: any
+  @spec simulate(robot :: Robot | {:error, String.t()}, instructions :: String.t()) :: Robot | {:error, String.t()}
   def simulate(@invalid_instruction, _) do
     @invalid_instruction
   end
-  
+
   def simulate(robot, "") do
     robot
   end
 
   def simulate(robot, instructions) do
-    simulate(move(robot, List.first(to_charlist instructions)), String.slice(instructions, 1..-1))
+    simulate(
+      move(robot, List.first(to_charlist(instructions))),
+      String.slice(instructions, 1..-1)
+    )
   end
 
-
-  def move(%{:dir => :north, :pos => {x, y}}, @commandA) do
+  @spec move(robot :: Robot, command :: any) :: Robot | {:error, String.t()}
+  def move(%Robot{:dir => :north, :pos => {x, y}}, @commandA) do
     create(:north, {x, y + 1})
   end
 
-  def move(%{:dir => :north, :pos => {x, y}}, @commandL) do
+  def move(%Robot{:dir => :north, :pos => {x, y}}, @commandL) do
     create(:west, {x, y})
   end
 
-  def move(%{:dir => :north, :pos => {x, y}}, @commandR) do
+  def move(%Robot{:dir => :north, :pos => {x, y}}, @commandR) do
     create(:east, {x, y})
   end
 
-  def move(%{:dir => :east, :pos => {x, y}}, @commandA) do
+  def move(%Robot{:dir => :east, :pos => {x, y}}, @commandA) do
     create(:east, {x + 1, y})
   end
 
-  def move(%{:dir => :east, :pos => {x, y}}, @commandL) do
+  def move(%Robot{:dir => :east, :pos => {x, y}}, @commandL) do
     create(:north, {x, y})
   end
 
-  def move(%{:dir => :east, :pos => {x, y}}, @commandR) do
+  def move(%Robot{:dir => :east, :pos => {x, y}}, @commandR) do
     create(:south, {x, y})
   end
 
-  def move(%{:dir => :south, :pos => {x, y}}, @commandA) do
+  def move(%Robot{:dir => :south, :pos => {x, y}}, @commandA) do
     create(:south, {x, y - 1})
   end
 
-  def move(%{:dir => :south, :pos => {x, y}}, @commandL) do
+  def move(%Robot{:dir => :south, :pos => {x, y}}, @commandL) do
     create(:east, {x, y})
   end
 
-  def move(%{:dir => :south, :pos => {x, y}}, @commandR) do
+  def move(%Robot{:dir => :south, :pos => {x, y}}, @commandR) do
     create(:west, {x, y})
   end
 
-  def move(%{:dir => :west, :pos => {x, y}}, @commandA) do
+  def move(%Robot{:dir => :west, :pos => {x, y}}, @commandA) do
     create(:west, {x - 1, y})
   end
 
-  def move(%{:dir => :west, :pos => {x, y}}, @commandL) do
+  def move(%Robot{:dir => :west, :pos => {x, y}}, @commandL) do
     create(:south, {x, y})
   end
 
-  def move(%{:dir => :west, :pos => {x, y}}, @commandR) do
+  def move(%Robot{:dir => :west, :pos => {x, y}}, @commandR) do
     create(:north, {x, y})
   end
 
@@ -114,22 +121,21 @@ defmodule RobotSimulator do
     @invalid_instruction
   end
 
-  
   @doc """
   Return the robot's direction.
 
   Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
-  @spec direction(robot :: any) :: atom
+  @spec direction(robot :: Robot) :: atom
   def direction(robot) do
-    robot[:dir]
+    robot.dir
   end
 
   @doc """
   Return the robot's position.
   """
-  @spec position(robot :: any) :: {integer, integer}
+  @spec position(robot :: Robot) :: {integer, integer}
   def position(robot) do
-    robot[:pos]
+    robot.pos
   end
 end
