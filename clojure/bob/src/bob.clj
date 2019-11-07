61fdff56ee9cf2af592c661d5967ca15be7f0041
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-11-08 00:58:06 +0700

    Remove unnecessary function and calls

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -2,17 +2,14 @@
   (:require [clojure.string :as str]))
 
 (defn remove-unword-chars [s]
-  (apply str (filter (fn [c] (or (= c \?) (Character/isLetter c))) s)))
+  (apply str (filter (fn [c] (Character/isLetter c)) s)))
 
 (defn shouting? [s]
   (let [clean-s (remove-unword-chars s)]
     (and (= clean-s (str/upper-case clean-s)) (> (count clean-s) 0))))
 
 (defn questioning? [s]
-  (str/ends-with? (remove-unword-chars s) "?"))
-
-(defn saying? [s]
-  (> (count (remove-unword-chars s)) 1))
+  (str/ends-with? s "?"))
 
 (defn nothing? [s]
   (str/blank? s))
@@ -20,7 +17,7 @@
 (defn response-for [s]
     (cond
       (nothing? s) "Fine. Be that way!"
-      (and (saying? s) (questioning? s) (shouting? s)) "Calm down, I know what I'm doing!"
+      (and (questioning? s) (shouting? s)) "Calm down, I know what I'm doing!"
       (questioning? s) "Sure."
       (shouting? s) "Whoa, chill out!"
       :else "Whatever.")
