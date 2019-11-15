c4374ecfcb0c4ed622b66a80ae5ccfb36f08e46b
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-11-15 12:27:14 +0700

    Use sprintf instead of String.Format

--- fsharp/beer-song/BeerSong.fs
+++ fsharp/beer-song/BeerSong.fs
@@ -7,14 +7,13 @@ let beerLeftVerse (numberOfBeer: int): string =
     | 1 -> "Take one down and pass it around, 1 bottle of beer on the wall."
     | 0 -> "Take it down and pass it around, no more bottles of beer on the wall."
     | numberOfBeer when numberOfBeer < 0 -> "Go to the store and buy some more, 99 bottles of beer on the wall."
-    | _ -> String.Format("Take one down and pass it around, {0} bottles of beer on the wall.", numberOfBeer)
+    | _ -> sprintf "Take one down and pass it around, %d bottles of beer on the wall." numberOfBeer
 
 let beerOnTheWallVerse (numberOfBeer: int): string =
     match numberOfBeer with
     | 1 -> "1 bottle of beer on the wall, 1 bottle of beer."
     | 0 -> "No more bottles of beer on the wall, no more bottles of beer."
-    | _ -> String.Format("{0} bottles of beer on the wall, {0} bottles of beer.", numberOfBeer)
-
+    | _ -> sprintf "%d bottles of beer on the wall, %d bottles of beer." numberOfBeer numberOfBeer
 let verseOf (numberOfBeer: int): List<string> =
     [ ""
       beerOnTheWallVerse numberOfBeer
